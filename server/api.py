1511e548e60ff046cf51646553e5374e8affc896
Harsh Patel <58038014+harshpatel0@users.noreply.github.com>
2021-11-26 13:45:49 +0300

    Split Base API code to another file

---
+++ server/api.py
@@ -0,0 +1,58 @@
+import os
+
+class API():
+  name = ""
+  properties = {}
+  version = 0
+
+  def respond(self, query):
+
+    if self.name == "" or self.properties == {} or self.version == 0:
+      return "API not initialized"
+    if query == "ping":
+      return 1
+    if query == "properties":
+      return self.properties
+    if query == "name":
+      return self.name
+    if query == "version":
+      return self.version
+  
+  def retrieve_cloudsave(self, uuid):
+    try:
+      with open(f'cloudsaves/{uuid}.sav', 'rb') as cloud_save_file:
+        cloudsave_data = cloud_save_file.read()
+
+      return cloudsave_data
+      
+    except FileNotFoundError:
+      return 'NOTFOUND'
+    
+  
+  def put_cloudsave(self, uuid, data):
+    data = data.encode('utf-8')
+    with open(f'cloudsaves/{uuid}.sav', 'wb') as cloud_save_file:
+      cloud_save_file.write(data)
+    
+  def delete_cloudsave(self, uuid):
+    try:
+      os.remove(f'{os.getcwd()}/cloudsaves/{uuid}.sav')
+      return 1
+    except Exception:
+      return "NOTFOUND"
+    
+  def patch_cloudsave(self, uuid, data):
+
+    data = data.encode('utf-8')
+    try:
+      save_file = open(f"cloudsaves/{uuid}.sav", 'rb')
+      save_file.close()
+
+    except Exception:
+      return "NOTFOUND"
+
+    os.remove(f"{os.getcwd()}/cloudsaves/{uuid}.sav")
+    with open(f'cloudsaves/{uuid}.sav', 'wb') as file:
+      file.write(data)
+    
+    return None
