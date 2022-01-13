26df62068eac95efbc20d8e6140b5767af42a9f9
Harsh Patel <58038014+harshpatel0@users.noreply.github.com>
2022-01-13 18:21:33 +0300

    Added property data fetcher function

---
+++ client/datahandler.py
@@ -0,0 +1,38 @@
+import requesthandler
+from cryptography.fernet import Fernet
+
+requesthandler = requesthandler.RequestHandler()
+
+class DataHandler:
+
+  key = requesthandler.get_decryption_key()
+
+  save_data = None
+  cryptographyhandler = Fernet(key)
+
+  def fill_data(self, server_url, username, data):
+    requesthandler.server_url = server_url
+    requesthandler.username = username
+    requesthandler.data = data
+
+    return None
+
+  def get_save_file(self):
+    save_file = requesthandler.cloudsave_get()
+    return save_file
+
+  def decrypt_encrypted_save(self, encrypted_save_file):
+    
+    encrypted_save_file = self.get_save_file()
+  
+    self.save_data = self.cryptographyhandler.decrypt(encrypted_save_file)
+    return self.save_data
+  
+  def encrypt_save_file(self):
+    return self.cryptographyhandler.encrypt(self.save_data)
+  
+  def save_user_data(self, data):
+    self.save_data = data
+  
+  def get_property_data(self):
+    return requesthandler.get_property_data()
