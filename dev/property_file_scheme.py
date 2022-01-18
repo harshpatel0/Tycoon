9b4f98f6673da662b22245497c44e076750357ef
Harsh Patel <58038014+harshpatel0@users.noreply.github.com>
2022-01-18 21:59:58 +0300

    Added property data scheme

---
+++ dev/property_file_scheme.py
@@ -0,0 +1,29 @@
+properties = {
+  "PropertyName": {
+    "dailyExpenses": 750,
+    "dailyProfit": 975,
+    "cost": 8980,
+
+    # Location and set doesn't matter and 
+    # doesn't affect any business stats
+
+    "location": "PropertyLocation",
+    "set": "PropertySet"
+  },
+
+  "PropertyName2": {
+    "dailyExpenses": 975,
+    "dailyProfit": 1250,
+    "cost": 10001,
+
+    # Location and set doesn't matter and 
+    # doesn't affect any business stats
+
+    "location": "PropertyLocation",
+    "set": "PropertySet"
+  }
+}
+
+"""
+  Property data is always sent from the server
+"""
