dec1e3b2bc15abbb68eba7f9ef3779998ef91d0d
Harsh Patel <58038014+harshpatel0@users.noreply.github.com>
2022-01-25 15:08:34 +0300

    Using the new save data DataHandler function

--- client/uielements/textboxes.test.py
+++ client/uielements/textboxes.test.py
@@ -4,12 +4,12 @@ import curses
 def alphanumerictextbox(screen):
   alphanumerictextbox = textboxes.AlphaNumericTextBox(screen)
 
-  print(alphanumerictextbox.initialize_textbox("Alpha Numeric TextBox", (1,1), 16))
+  print(alphanumerictextbox.create_textbox("Alpha Numeric TextBox", (1,1), 16))
 
 def numerictextbox(screen):
   numerictextbox = textboxes.NumericTextBox(screen)
 
-  print(numerictextbox.initialize_textbox("Numeric TextBox", (1,1), 16))
+  print(numerictextbox.create_textbox("Numeric TextBox", (1,1), 16))
 
 choice = input("1: Alphanumeric, 2: Numeric\n\t")
 
