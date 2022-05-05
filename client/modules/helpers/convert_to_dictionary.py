8b2af2f1fb0d31588927ab652bed5a936bbf1183
Harsh Patel <58038014+harshpatel0@users.noreply.github.com>
2022-05-05 18:55:41 +0300

    Moved convert_to_dictionary to helper folder

---
+++ client/modules/helpers/convert_to_dictionary.py
@@ -0,0 +1,19 @@
+import logging
+import json
+
+# Intialize Logger
+# logging.basicConfig(filename="converttodictionaryhelper.log", format='%(asctime)s %(message)s', filemode='w')
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+def convert_to_dictionary(data, name_of_data = None):
+  # name_of_data is used for logging
+  logger.debug(f"Inital data before converting to dictionary: {data}, Name of data: {name_of_data}")
+    
+  data = data.replace('\'', '\"')
+  logger.debug(f"After double quotting: {data}, Name of data: {name_of_data}")
+
+  data = json.loads(data)
+  logger.debug(f"JSON Module Output of Save File: {data}, Name of data: {name_of_data}")
+
+  return data
