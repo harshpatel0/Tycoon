e802710582820f394adb223542570133889cc505
Harsh Patel <58038014+harshpatel0@users.noreply.github.com>
2022-10-03 22:16:58 +0300

    Changes to some API endpoints

--- client/modules/datahandlers/requesthandler.py
+++ client/modules/datahandlers/requesthandler.py
@@ -117,6 +117,6 @@ class RequestHandler:
     return request.text
     
   def get_server_version(self):
-    request = self.query_data('api/server/server-version')
+    request = self.query_data('api/server/version')
     if request.text == "NO_CONNECTION": return "NO_CONNECTION"
     return float(request.text)
