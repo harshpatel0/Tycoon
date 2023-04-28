7be08d92a382eeab2ba31abf6e77a27de12b0b45
Harsh Patel <58038014+harshpatel0@users.noreply.github.com>
2023-04-28 21:39:45 +0300

    Fixed a bug where the prompt is loaded before expected

--- server/modules/console.py
+++ server/modules/console.py
@@ -5,7 +5,7 @@ import traceback
 logger = logging.getLogger()
 
 class Console:
-    def __init__(self, keyhandler, cloudsavehandler, basicshandler, propertyhandler, keyhandlerui, enable_console=False) -> None:
+    def __init__(self, keyhandler, cloudsavehandler, basicshandler, propertyhandler, keyhandlerui, propertyhandlerui, enable_console=False) -> None:
         if enable_console == False:
            logger.warn("The console is disabled, the initialization of the console failed successfully")
            print("The console is disabled and the handlers were not initialized, the prompt will not be displayed")
@@ -16,11 +16,10 @@ class Console:
             self.basicshandler = basicshandler
             self.propertyhandler = propertyhandler
             self.keyhandlerui = keyhandlerui
+            self.propertyhandlerui = propertyhandlerui
 
             logger.info("The console has been initialized successfully and all handlers have been passed to it.")
             print(f"The console was initialized successfully, a prompt will be displayed")
-
-            self.prompt()
     
     def reloadproperties(self):
         logger.info("[Console] Force reloaded properties")
@@ -44,23 +43,56 @@ class Console:
     def showkeyhandlerui(self):
         self.keyhandlerui.dashboard()
     
+    def showpropertyhandlerui(self):
+        self.propertyhandlerui.dashboard()
+    
+    # def prompt(self):
+    #     while True:
+    #         logger.debug("Console Prompt Shown")
+    #         prompt = input("> ")
+
+    #         if prompt == 'reloadproperties':
+    #             self.reloadproperties()
+    #             self.prompt()
+    #         if prompt == 'resetpropertyticks':
+    #             self.resetpropertyticks()
+    #             self.prompt()
+    #         if prompt == 'setpropertyticks':
+    #             self.setpropertyticks()
+    #             self.prompt()
+    #         if prompt == 'showkeyhandlerui':
+    #             self.showkeyhandlerui()
+    #             self.prompt()
+    #         if prompt == 'showpropertyhandlerui':
+    #             self.showpropertyhandlerui()
+    #             self.prompt()
+    #         if prompt == 'stop' or 'quit':
+    #             quit()
+    #         else:
+    #             logger.warn(f"[Console] The console is now running untrusted, arbitary python code!\n\tCommand:{prompt}")
+    #             prompt.encode()
+    #             try:
+    #                 exec(prompt)
+    #                 self.prompt()
+    #             except Exception:
+    #                 logger.exception(f"[Console] Console arbitary code threw an exception, \n{Exception}")
+    #                 traceback.print_exc()
+    #                 self.prompt()
+    #             except KeyboardInterrupt:
+    #                 print("Closed the server")
+    #                 quit()
+
     def prompt(self):
         while True:
+            logger.debug("[Console] Reached prompt")
             prompt = input("> ")
-
-            if prompt == 'reloadproperties':
-                self.reloadproperties()
-            if prompt == 'resetpropertyticks':
-                self.resetpropertyticks()
-            if prompt == 'setpropertyticks':
-                self.setpropertyticks()
-            if prompt == 'showkeyhandlerui':
-                self.showkeyhandlerui()
-            else:
-                logger.warn(f"[Console] The console is now running untrusted, arbitary python code!\n\tCommand:{prompt}")
-                prompt.encode()
-                try:
-                    exec(prompt)
-                except Exception:
-                    logger.exception(f"[Console] Console arbitary code threw an exception, \n{Exception}")
-                    traceback.print_exc()
+            
+            logger.warn(f"The console is running the command\n\t\t{prompt}")
+            prompt = prompt.encode()
+            try:
+                exec(prompt)
+                continue
+            except Exception:
+                logger.exception(f'[Console] Prompt threw an exception\n\n{Exception}')
+                traceback.print_exc()
+                continue
