d60acc1dec628d40ac9faceb5a9b2a9422e874ae
Harsh Patel <58038014+harshpatel0@users.noreply.github.com>
2024-04-25 22:32:37 +0300

    Main HTML for server connections page

---
+++ client/web/static/mainpage.js
@@ -0,0 +1,7 @@
+function closeAlert() {
+  document.getElementById("error-message").style.display = "none";
+}
+
+function debugShowAlert() {
+  document.getElementById("error-message").style.display = "block";
+}
